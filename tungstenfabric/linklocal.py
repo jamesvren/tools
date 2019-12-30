d888a9e3b6f207f24c51fda24f7ed1961e0a365f
james <renjinyao@huayun.com>
2019-12-30 23:39:54 +0800

    add compute host name

--- tungstenfabric/linklocal.py
+++ tungstenfabric/linklocal.py
@@ -8,8 +8,15 @@ def get_vm_info(vrouter_ip='localhost'):
     vm_info = PrettyTable()
     vm_info.field_names = ["Compute Node", "VM Name", "VM IP Address", "Link-Local Address"]
     
+    os.popen('curl -s http://' + vrouter_ip + ':8085/Snh_VrouterInfoReq > vrouter_name.xml')
     os.popen('curl -s http://' + vrouter_ip + ':8085/Snh_ItfReq > ' + vrouter_ip + '.xml')
     
+    vrouter_tree = ET.parse('vrouter_name.xml')
+    vrouter_root = vrouter_tree.getroot()
+    os.remove('vrouter_name.xml')
+    vrouter_name = vrouter_root.find('display_name').text
+    compute_name = vrouter_name + ' [' + vrouter_ip + ']'
+
     compute_tree = ET.parse(vrouter_ip + '.xml')
     compute_root = compute_tree.getroot()
     os.remove(vrouter_ip + '.xml')
@@ -20,7 +27,7 @@ def get_vm_info(vrouter_ip='localhost'):
         mdata_ip_addr = interface.find('mdata_ip_addr').text
     
         if vm_name is not None:
-            vm_info.add_row([vrouter_ip, vm_name, ip_addr, mdata_ip_addr])
+            vm_info.add_row([compute_name, vm_name, ip_addr, mdata_ip_addr])
     return vm_info
 
 def main():
